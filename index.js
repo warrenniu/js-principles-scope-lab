24a43a6b0d7cc75f3635ea07b35f1c4da6683bc6
warrenniu <warrenniu@gmail.com>
2020-11-15 10:39:33 -0500

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'warren'
+
+function upperCaseCustomerName() {
+    customerName = 'BOB';
+    return customerName;
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'bob';
+    return leastFavoriteCustomer;
+}
